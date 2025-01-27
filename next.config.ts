84db1241b9a4b1018ea1aae42df63ef638aea8df
AnirbanPa1 <palanirban2003@gmail.com>
2025-01-27 18:13:29 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['cloud.appwrite.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'png.pngtree.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloud-appwrite.io'
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+    ]
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb', // Adjust the size as needed
+    },
+  },
 };
 
 export default nextConfig;
